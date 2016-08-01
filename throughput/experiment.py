2cb0d816d83e2e19e69c2b84105cb1c08ba1d63a
megfault <ana@megfau.lt>
2016-08-01 15:46:30 +0200

    change experiment config and logging

--- throughput/experiment.py
+++ throughput/experiment.py
@@ -4,10 +4,11 @@ from argparse import ArgumentParser
 from atexit import register
 
 from pyadtn.aDTN import aDTN
+from pyadtn.utils import info, debug
 
-SENDING_FREQS = [5, 10, 30, 60]
-BATCH_SIZE = [1, 10]
-EXPERIMENT_DURATION = 10 * 60  # 10 minutes in seconds
+SENDING_FREQS = [10]
+BATCH_SIZE = [10]
+EXPERIMENT_DURATION = 1 * 60  # 1 minute in seconds
 IFACE = "wlan0"
 FREQ = str(2432)  # 802.11 channel 1
 
@@ -26,7 +27,7 @@ if __name__ == "__main__":
             # Inform about current config.
             experiment_id = "throughput_" + "_".join(
                 [str(i) for i in ["bs", bs, "sf", sf, "cr"]])
-            print("Now running: {}".format(experiment_id))
+            info("\nNow running: {}".format(experiment_id))
 
             # Start aDTN
             adtn = aDTN(bs, sf, IFACE, experiment_id)
