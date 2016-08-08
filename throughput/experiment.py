ae93b186922ca19ab3b65ccf577f031e75c402ed
megfault <ana@megfau.lt>
2016-08-08 16:06:23 +0200

    pass parameters via CLI instead of fixed set

--- throughput/experiment.py
+++ throughput/experiment.py
@@ -6,8 +6,6 @@ from atexit import register
 from pyadtn.aDTN import aDTN
 from pyadtn.utils import info, debug
 
-SENDING_FREQS = [60, 30, 15, 1]
-BATCH_SIZE = [1, 10]
 EXPERIMENT_DURATION = 5 * 60 + 10 # 5 minutes and 5 seconds (in seconds)
 IFACE = "wlan0"
 FREQ = str(2432)  # 802.11 channel 1
@@ -15,24 +13,27 @@ FREQ = str(2432)  # 802.11 channel 1
 if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument('device_id', type=str, help='the hostname of this device')
+    parser.add_argument('sending_freq', type=int, help='time between sending of a batch (in seconds)')
+    parser.add_argument('batch_size', type=int, help='batch size (in number of packets)')
     args = parser.parse_args()
 
     device_id = args.device_id
+    sf = args.sending_freq
+    bs = args.batch_size
 
     call(("./network-setup.sh", IFACE))
     call(["iw", IFACE, "ibss", "join", "test", FREQ])
 
-    for bs in BATCH_SIZE:
-        for sf in SENDING_FREQS:
-            # Inform about current config.
-            experiment_id = "throughput_" + "_".join(
-                [str(i) for i in ["bs", bs, "sf", sf, "cr"]])
-            info("\nNow running: {}".format(experiment_id))
 
-            # Start aDTN
-            adtn = aDTN(bs, sf, IFACE, experiment_id)
-            adtn.start()
+    # Inform about current config.
+    experiment_id = "throughput_" + "_".join(
+        [str(i) for i in ["bs", bs, "sf", sf, "cr"]])
+    info("\nNow running: {}".format(experiment_id))
 
-            sleep(EXPERIMENT_DURATION)
+    # Start aDTN
+    adtn = aDTN(bs, sf, IFACE, experiment_id)
+    adtn.start()
 
-            adtn.stop()
+    sleep(EXPERIMENT_DURATION)
+
+    adtn.stop()
