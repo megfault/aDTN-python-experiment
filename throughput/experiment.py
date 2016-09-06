0e6e529493bf85838cc98c7f14efc69c8763ab23
megfault <ana@megfau.lt>
2016-09-06 13:44:25 +0200

    pass experiment duration via command line

--- throughput/experiment.py
+++ throughput/experiment.py
@@ -4,7 +4,6 @@ from argparse import ArgumentParser
 
 from pyadtn.aDTN import aDTN
 
-EXPERIMENT_DURATION = 5 * 60 # 5 minutes (in seconds)
 IFACE = "wlan0"
 FREQ = str(2432)  # 802.11 channel 5
 
@@ -13,11 +12,13 @@ if __name__ == "__main__":
     parser.add_argument('device_id', type=str, help='the hostname of this device')
     parser.add_argument('sending_freq', type=float, help='time between sending of a batch (in seconds)')
     parser.add_argument('batch_size', type=int, help='batch size (in number of packets)')
+    parser.add_argument('duration', type=int, help='experiment duration (in seconds)')
     args = parser.parse_args()
 
     device_id = args.device_id
     sf = args.sending_freq
     bs = args.batch_size
+    experiment_duration = args.duration
 
     call(("./network-setup.sh", IFACE))
     call(["iw", IFACE, "ibss", "join", "test", FREQ])
@@ -31,6 +32,6 @@ if __name__ == "__main__":
     adtn = aDTN(bs, sf, IFACE, experiment_id)
     adtn.start()
 
-    sleep(EXPERIMENT_DURATION)
+    sleep(experiment_duration)
 
     adtn.stop()
